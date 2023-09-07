573835431df535cb7ce58d27961bec98c67e0d9f
root <root@uwmelk.edu.pl>
2023-09-07 18:50:18 +0000

    Custom check certificate page and fonts

--- classes/checkCertificate.php
+++ classes/checkCertificate.php
@@ -54,18 +54,18 @@ $decrypted = srCertificateDigitalSignature::decryptSignature(strtr($_GET['signat
 
                                 <?php if ($decrypted) { ?>
                                     <div class="alert alert-info">
-                                        <h2>CHECK CERTIFICATE SIGNATURE</h2>
-                                        <h5 class="ilAccHeadingHidden"><a id="il_message_focus" name="il_message_focus">Informationsmeldung</a>
+                                        <h2>SPRWADŹ WAŻNOŚĆ CERTYFIKATU</h2>
+                                        <h5 class="ilAccHeadingHidden"><a id="il_message_focus" name="il_message_focus" style="color:green">Certyfikat jest ważny.</a>
                                         </h5>
-                                        <p>The decryption was successful.<br/><?php echo $decrypted; ?></p>
+                                        <p>Odszyfrowanie powiodło się.<br/><?php echo $decrypted; ?></p>
                                     </div>
                                 <?php } else { ?>
                                     <div class="alert alert-warning">
-                                        <h2>CHECK CERTIFICATE SIGNATURE</h2>
+                                        <h2>SPRWADŹ WAŻNOŚĆ CERTYFIKATU</h2>
                                         <h5 class="ilAccHeadingHidden">
-                                            <a id="il_message_focus" name="il_message_focus">Fehlermeldung</a>
+                                            <a id="il_message_focus" name="il_message_focus" style="color:red;">Certyfikat jest nieważny.<a/>
                                         </h5>
-                                        <p>The signature value could not be decrypted.</p>
+                                        <p>Nie można odszyfrować certyfikatu.</p>
                                     </div>
                                 <?php } ?>
                             </div>
--- plugin.php
+++ plugin.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
+#require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
+
+declare(strict_types=1);
 
 $id = 'cert';
-$version = '3.0.1';
-$ilias_min_version = '5.4.0';
-$ilias_max_version = '6.999';
+$version = '4.0.1';
+$ilias_min_version = '8.0';
+$ilias_max_version = '8.999';
 $responsible = 'studer + raimann ag - Team Custom 1';
 $responsible_mail = 'support-custom1@studer-raimann.ch';
